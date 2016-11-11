4f9eed897c968eedcb49e2ef1d037d9494722258
viniciusofp <viniciusofp@gmail.com>
2016-11-11 11:00:45 -0200

    scroll + lento

--- js/main.js
+++ js/main.js
@@ -5,3 +5,30 @@ $('#circulo').waypoint(function(direction) {
 },{
   offset:'60px'
 });
+
+
+
+
+// =======
+
+if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
+window.onmousewheel = document.onmousewheel = wheel;
+
+function wheel(event) {
+    var delta = 0;
+    if (event.wheelDelta) delta = event.wheelDelta / 120;
+    else if (event.detail) delta = -event.detail / 3;
+
+    handle(delta);
+    if (event.preventDefault) event.preventDefault();
+    event.returnValue = false;
+}
+
+function handle(delta) {
+    var time = 250;
+	var distance = 800;
+    
+    $('html, body').stop().animate({
+        scrollTop: $(window).scrollTop() - (distance * delta)
+    }, time );
+}
